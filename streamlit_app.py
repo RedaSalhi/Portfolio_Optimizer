4494bb654624cfd76cfe1d3c0fe73738fc8bf781
RedaSalhi <salhi.reda47@gmail.com>
2025-06-05 19:50:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title("🚀 Welcome to My Streamlit App")
+st.title("🚀 Welcome to My App")
 st.markdown("Choose a section to continue:")
 
 col1, col2 = st.columns(2)
