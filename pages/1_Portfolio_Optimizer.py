0e40abca2b14415478e4e40f6ef622fb21f77bd3
RedaSalhi <salhi.reda47@gmail.com>
2025-06-05 01:50:05 +0100

    Update 1_Portfolio_Optimizer.py

--- pages/1_Portfolio_Optimizer.py
+++ pages/1_Portfolio_Optimizer.py
@@ -4,7 +4,13 @@ import streamlit as st
 import plotly.graph_objects as go
 from optimizer import optimize_portfolio
 
+st.set_page_config(page_title="Portfolio Optimizer", layout="wide")
+
+if st.button("🔙 Back to Home"):
+    st.switch_page("streamlit_app.py")
+
 st.title("📈 Portfolio Optimizer using Markowitz & CAPM")
+
 
 with st.sidebar:
     st.header("⚙️ User Inputs")
